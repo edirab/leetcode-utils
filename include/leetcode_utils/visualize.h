262bedc5a93c66cedec90b1a999e7ecb533a1fe6
Evgenii Andreev <edirab@yandex.ru>
2022-07-10 20:52:55 +0200

    - Added tree visualization in dot lang

---
+++ include/leetcode_utils/visualize.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "TreeNode_int.h"
+#include <iostream>
+#include <string>
+#include <stack>
+//#include <format> // C++ 20
+
+using std::cout;
+using std::string;
+using std::stack;
+
+class Visualizer
+{
+    public:
+        string get(TreeNode* root, bool draw_nulls=true);
+
+    private:
+
+};
