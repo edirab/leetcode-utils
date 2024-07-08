d495cb755350c4449e0a6cd376dbb2b8103186e2
Evgenii Andreev <edirab@yandex.ru>
2024-07-09 00:25:15 +0200

    Added a common header

---
+++ include/leetcode_utils/leetcode_utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "arrays/random_ints.h"
+
+#include "lists/generator.h"
+
+#include "trees/codec.h"
+#include "trees/generator.h"
+#include "trees/traversals.h"
+#include "trees/visualize.h"
+
+#include "trees/utils/find.h"
